2bc5f262132d7ec1c0ed0198d37f4079960df898
Greg Lockwood <greglok@optusnet.com.au>
2009-09-28 01:59:37 +1000

    Refactored a little bit to at least create several partials for 'people'; Added ability to customise divider - will add this to Settings stored in DB next.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
   def yes_or_no(bool_value)
     bool_value ? "Yes" : "No"
   end
+  
+  def divider
+    params[:divider] ||= "line"
+    case params[:divider]
+    when "line"
+      "<div class='divider'></div>"
+    when "blanks"
+      "<p>&nbsp;</p>"
+    end
+  end
 end
